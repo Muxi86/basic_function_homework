c584efba02316c93e8c59a3323027693163a1e9c
Muxiddin86 <imamberdievmuxiddin@gmail.com>
2022-12-18 17:56:10 +0500

    f08

--- basic_func08.py
+++ basic_func08.py
@@ -1,5 +1,8 @@
 # Create a function called main.
-
+def main(a):
+    a = a**2/a
+    return a
+print(main(int(input("a ="))))
 # Create function arguments a.
 
 # Increase the value of a to one and return.
